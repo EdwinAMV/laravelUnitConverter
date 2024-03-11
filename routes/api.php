247c71706a86b710a7e61b7d45d062c3a384ff24
EdwinAMV <edalbert1407@gmail.com>
2024-03-11 20:24:27 +0100

    Fix api routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\ConvertLengthController;
+use App\Http\Controllers\ConvertSpeedController;
+use App\Http\Controllers\ConvertTemperatureController;
+use App\Http\Controllers\ConvertVolumeController;
+use App\Http\Controllers\ConvertWeightController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +22,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/convert/length/{value}/{unit}', ConvertLengthController::class);
+Route::get('/convert/weight/{value}/{unit}', ConvertWeightController::class);
+Route::get('/convert/temperature/{value}/{unit}', ConvertTemperatureController::class);
+Route::get('/convert/volume/{value}/{unit}', ConvertVolumeController::class);
+Route::get('/convert/speed/{value}/{unit}', ConvertSpeedController::class);
